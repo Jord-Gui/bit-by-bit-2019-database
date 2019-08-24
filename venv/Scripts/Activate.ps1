acbd6fa60b11973286d977e5cdce6cb2107c7196
stevenhvtran <stransteven@outlook.com>
2019-08-24 20:39:13 +1000

    Removed virtual environment from repo

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Jord_\Desktop\bit-by-bit-2019-database\venv"
+$env:VIRTUAL_ENV="C:\Users\stran\Documents\PROGRAMMING\bit-by-bit-2019-database\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
